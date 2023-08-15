e20bfa0039364aa26f2577d2ec373134d1056582
gabriellelailler <gabrielle.lailler@gmail.com>
2023-08-15 21:46:31 +0200

    page 404

--- frontend/src/components/Card/index.jsx
+++ frontend/src/components/Card/index.jsx
@@ -1,14 +1,21 @@
 import logements from '../../logements.json'
 import '../../styles/card.sass'
 import { useParams } from 'react-router-dom'; // hook permettant d'obtenir les paramètres d'URL
-
+import '../../styles/error.sass'
 
 function  Card() {
   const { id } = useParams(); // -> si l'URL est "/article/123", id sera égal à "123".
   const card = logements.find(logement => logement.id === id); // .find() pour rechercher l'article correspondant dans le tableau logements en utilisant l'ID que nous avons extrait des paramètres d'URL. 
 
   if (!card) {
-    return <div>Désolé, l'appartement recherché ne semble plus disponible ...</div>;
+    return (
+      <div className="error">
+            <h1>404</h1>
+            <h2>Oups! La page que vous demandez n'existe pas</h2>
+            <p><a href="/">Retourner sur la page d'accueil</a></p>
+        </div>
+    )
+    
   }
 
   return (
--- frontend/src/pages/Error/index.jsx
+++ frontend/src/pages/Error/index.jsx
@@ -1,8 +1,11 @@
+import '../../styles/error.sass'
 
 function Error() {
     return (
-        <div>
-            <h1>Oups! La page que vous demandez n'existe pas</h1>
+        <div className="error">
+            <h1>404</h1>
+            <h2>Oups! La page que vous demandez n'existe pas</h2>
+            <p><a href="/">Retourner sur la page d'accueil</a></p>
         </div>
     )
 }
